f16963c8119502a0066e265ea16e90104eb76eae
tarathania <100033376+tarathania@users.noreply.github.com>
2022-05-27 22:35:05 +0700

    php

--- config/Database.php
+++ config/Database.php
@@ -1,20 +1,24 @@
-<?php
-    class Database{
-        private $host = 'localhost';
-        private $db_name = 'Shafitabil_shop';
-        private $username = 'root';
-        private $password = '';
-        private $conn;
+<?php 
+  class Database {
+    // DB Params
+    private $host = 'localhost';
+    private $db_name = 'Shafitabil';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
 
-        public function connect(){
-            $this->conn = null;
+    // DB Connect
+    public function connect() {
+      $this->conn = null;
 
-            try{
-                $this->conn = new PDD('msql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-                $this->conn->setAttribute(PDD::ATTR_ERRMODE, PDD::ERRMODE_EXCEPTION);
-            }catch(PDDException $e){
-                echo 'Connection Error:' . $e->getMessage();
-            }
-            return $this->conn;
-        }
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
+
+      return $this->conn;
     }
+  }
+© 2022 GitHub, Inc.
